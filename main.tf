79e3bec1f2f6b1bba8e89f43b31a3335c0d205ee
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-29 15:14:16 +0000

    all

--- main.tf
+++ main.tf
@@ -1,12 +1,52 @@
-//resource "aws_docdb_cluster" "main" {
-//  cluster_identifier      = "${var.env}-docdb"
-//  engine                  = var.engine
-//  master_username         = data.aws_ssm_parameter.user.value
-//  master_password         = data.aws_ssm_parameter.pass.value
-//  backup_retention_period = var.backup_retention_period
-//  preferred_backup_window = var.preferred_backup_window
-//  skip_final_snapshot     = true
-//}
+resource "aws_docdb_cluster" "main" {
+  cluster_identifier      = "${var.env}-docdb"
+  engine                  = var.engine
+  engine_version          = var.engine_version
+  master_username         = data.aws_ssm_parameter.user.value
+  master_password         = data.aws_ssm_parameter.pass.value
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot     = var.skip_final_snapshot
+  db_subnet_group_name    = aws_docdb_subnet_group.main.name
+  kms_key_id              = data.aws_kms_key.key.arn
+  storage_encrypted       = var.storage_encrypted
+  vpc_security_group_ids  = [aws_security_group.main.id]
+}
+
+resource "aws_docdb_cluster_instance" "cluster_instances" {
+  count              = var.no_of_instances
+  identifier         = "${var.env}-docdb-${count.index}"
+  cluster_identifier = aws_docdb_cluster.main.id
+  instance_class     = var.instance_class
+}
+
+resource "aws_security_group" "main" {
+  name        = "docdb-${var.env}"
+  description = "docdb-${var.env}"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "DOCDB"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = var.allow_subnets
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    { Name = "docdb-${var.env}" }
+  )
+}
+
 
 resource "aws_docdb_subnet_group" "main" {
   name       = "${var.env}-docdb"
@@ -16,4 +56,22 @@ resource "aws_docdb_subnet_group" "main" {
     var.tags,
     { Name = "${var.env}-subnet-group" }
   )
+}
+
+resource "aws_ssm_parameter" "docdb_url_catalogue" {
+  name  = "${var.env}.docdb.url.catalogue"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}@dev-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url_user" {
+  name  = "${var.env}.docdb.url.user"
+  value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}@dev-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+  type  = "String"
+}
+
+resource "aws_ssm_parameter" "docdb_endpoint" {
+  name  = "${var.env}.docdb.endpoint"
+  type  = "String"
+  value = aws_docdb_cluster.main.endpoint
 }
--- vars.tf
+++ vars.tf
@@ -6,3 +6,11 @@ variable "skip_final_snapshot" {}
 variable "engine_version" {}
 variable "subnet_ids" {}
 variable "tags" {}
+
+variable "storage_encrypted" {
+  default = true
+}
+variable "no_of_instances" {}
+variable "instance_class" {}
+variable "allow_subnets" {}
+variable "vpc_id" {}
